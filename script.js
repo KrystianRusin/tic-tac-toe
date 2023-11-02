ace8b5219954591215dbdf235b39c27349452b01
KrystianRusin <krystian.r@hotmail.com>
2023-11-02 16:21:03 -0400

    refactored code and added place mark functionality

--- script.js
+++ script.js
@@ -1,17 +1,8 @@
-function gameBoard(p1, p2) {
-    const createPlayer = (name, mark, turn) => {
-        return {name, mark, turn}
-    }
-    
-    const player1 = createPlayer(p1, "X", true)
-    const player2 = createPlayer(p2, "O", false)
-    
-    const init = () => {
-        const startBtn = document.querySelector(".start-btn")
-        startBtn.addEventListener('click', createBoard)
-    }
-    
-    const createBoard = () => {
+(function (){
+    const createBoard = (event) => {
+        event.preventDefault()
+        const p1 = document.getElementById("p1-name").value
+        const p2 = document.getElementById("p2-name").value
         const container = document.querySelector(".container")
         container.innerHTML = ""
         const gamediv = document.createElement('div')
@@ -20,14 +11,73 @@ function gameBoard(p1, p2) {
             for (let j = 0; j < 3; j++) {
                 const cell = document.createElement('div');
                 cell.classList.add('grid-cell');
+                cell.dataset.cellId = i.toString() + j.toString()
                 gamediv.appendChild(cell);
             }
         }
         container.appendChild(gamediv)
-    }
+        gameBoard(p1, p2)
+        }
 
-    init()
+        const startBtn = document.querySelector(".start-btn")
+        startBtn.addEventListener('click', createBoard)
+  
+})()
+
+
+function gameBoard(p1, p2) {
+    const init = () => {
+        const player1 = player.createPlayer(p1, "X", true)
+        const player2 = player.createPlayer(p2, "O", false)
+        const cells = document.querySelectorAll(".grid-cell");
+        cells.forEach(cell => {
+            cell.addEventListener('click', function() {
+                game.placeMark(player1, cell)
+            });
+        })
+    }
     
+    init()
 }
 
-gameBoard()
+const player = (function(){
+
+    const createPlayer = (name, mark, turn) => {
+        return {name, mark, turn}
+    }
+
+    const getName = (p) => {
+        return p.name
+    }
+    const getMark = (p) => {
+        return p.mark
+    }
+
+    const getTurn = (p) => {
+        return p.turn
+    }
+    const setTurn = (p, turn) => {
+        p.turn = turn
+    }
+
+    return{
+        createPlayer,
+        getName,
+        getMark,
+        getTurn,
+        setTurn
+    }
+})();
+
+const game = (function() {
+
+    const placeMark = (p, cell) => {
+        let mark = player.getMark(p)
+        cell.innerHTML = mark
+    }
+
+    return {
+        placeMark
+    }
+})()
+
