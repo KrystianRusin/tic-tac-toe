1e7616f916ffe4df71b3bd0dba58c8b8c429fc86
KrystianRusin <krystian.r@hotmail.com>
2023-11-02 13:34:14 -0400

    game board is now created when user presses start game button

--- script.js
+++ script.js
@@ -1,10 +1,33 @@
-const gameBoard = () => {
-
+function gameBoard(p1, p2) {
     const createPlayer = (name, mark, turn) => {
         return {name, mark, turn}
     }
+    
+    const player1 = createPlayer(p1, "X", true)
+    const player2 = createPlayer(p2, "O", false)
+    
+    const init = () => {
+        const startBtn = document.querySelector(".start-btn")
+        startBtn.addEventListener('click', createBoard)
+    }
+    
+    const createBoard = () => {
+        const container = document.querySelector(".container")
+        container.innerHTML = ""
+        const gamediv = document.createElement('div')
+        gamediv.classList.add("game-container")
+        for (let i = 0; i < 3; i++) {
+            for (let j = 0; j < 3; j++) {
+                const cell = document.createElement('div');
+                cell.classList.add('grid-cell');
+                gamediv.appendChild(cell);
+            }
+        }
+        container.appendChild(gamediv)
+    }
 
-    const player1 = createPlayer("p1", "x", true)
-    const player2 = createPlayer("p2", "x", false)
-
+    init()
+    
 }
+
+gameBoard()
